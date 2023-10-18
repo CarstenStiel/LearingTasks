740c282d70adec687638a4201cb3ccd68c9afc1d
CarstenStiel <timothy.kriewald@gmail.com>
2023-10-18 16:34:32 +0200

    Updated comments

--- arangeTask.py
+++ arangeTask.py
@@ -5,12 +5,12 @@ def calculate_steps(start, stop, step):
     arr = []  # Initialisierung eines leeren Arrays
     current = start  # Derzeitiger Schritt initialisiert mit dem Start
 
-    if step <= 0 and start <= stop:
+    if step <= 0 and start <= stop:  # Fehler bei negativer Schrittweite und start kleiner stop
         raise ValueError("Die Schrittweite darf nicht 0 oder kleiner sein.")
-    elif step >= 0 and stop <= start:
+    elif step >= 0 and stop <= start:  # Fehler bei postiver Schrittweite und stop kleiner start
         raise ValueError("Start muss größer als Stop sein.")
     else:  # Ansonsten, führe folgende While-Schleife aus
-        while not current == stop:  # Solange, der derzetige Wert kleiner ist als stop führe Folgendes aus:
+        while not current == stop:  # Solange, der derzeitige Wert kleiner ist als stop führe Folgendes aus:
             arr.append(current)  # Füge denn derzeitigen Wert hinten das Array an
             current += step  # Erhöhe den derzeitigen Wert und die Schrittweite
 
--- main.py
+++ main.py
@@ -20,28 +20,28 @@ if __name__ == '__main__':
     print(arr2)
     print(len(arr2))
     """
-    """
     # Aufgabe SN
     # SN1
     res1Rec = sn.sn1Recursive(6)  # SN1 rekursiv berechnet
     res1For = sn.sn1For(6)  # SN1 mittels For-Schleife berechnet
     print("SN1-Genauigkeiten mit Rekursion:")  # Ausgabe
-    print(f"In einfacher Genauigkeit: {sn.singlePrecision(res1Rec)}")  # SN1 (rekusiv) in einfacher Genauigkeit berechnet (siehe snTask) und ausgegeben
-    print(f"In doppelter Genauigkeit: {sn.doublePrecision(res1Rec)}")  # SN1 (rekusiv) in doppelter Genauigkeit berechnet (siehe snTask) und ausgegeben
-    res1Rec = sn.sn1Recursive(6)  # SN1 rekursiv berechnet
-    res1For = sn.sn1For(6)  # SN1 mittels For-Schleife berechnet
+    print(f"In einfacher Genauigkeit: {sn.singlePrecision(res1Rec)}")  # SN1 (rekursiv) in einfacher Genauigkeit berechnet (siehe snTask) und ausgegeben
+    print(f"In doppelter Genauigkeit: {sn.doublePrecision(res1Rec)}")  # SN1 (rekursiv) in doppelter Genauigkeit berechnet (siehe snTask) und ausgegeben
     print("SN1-Genauigkeiten mit For-Schleife:")
     print(f"In einfacher Genauigkeit: {sn.singlePrecision(res1For)}")  # SN1 (For-Schleife) in einfacher Genauigkeit berechnet (siehe snTask) und ausgegeben
     print(f"In doppelter Genauigkeit: {sn.doublePrecision(res1For)}")  # SN1 (For-Schleife) in doppelter Genauigkeit berechnet (siehe snTask) und ausgegeben
+    """
     # SN2
     res2Rec = sn.sn2Recursive(2)  # SN2 rekursiv berechnet
     res2For = sn.sn2For(2)  # SN2 mittels For-Schleife berechnet
     print("SN2-Genauigkeiten:")
-    sn.precision(res2Rec)  # SN2 (rekusiv) in einfacher und doppelter Genauigkeit ausgeben (siehe snTask)
+    sn.precision(res2Rec)  # SN2 (rekursiv) in einfacher und doppelter Genauigkeit ausgeben (siehe snTask)
     sn.precision(res2For)  # SN2 (For-Schleife) in einfacher und doppelter Genauigkeit ausgeben (siehe snTask)
+    """
     """
     # SN1 Input
     res1Rec = sn.sn1Recursive(inputN())  # SN1 rekursiv berechnet
     print("SN1-Genauigkeiten mit Rekursion:")  # Ausgabe
-    print(f"In einfacher Genauigkeit: {sn.singlePrecision(res1Rec)}")  # SN1 (rekusiv) in einfacher Genauigkeit berechnet (siehe snTask) und ausgegeben
-    print(f"In doppelter Genauigkeit: {sn.doublePrecision(res1Rec)}")  # SN1 (rekusiv) in doppelter Genauigkeit berechnet (siehe snTask) und ausgegeben
+    print(f"In einfacher Genauigkeit: {sn.singlePrecision(res1Rec)}")  # SN1 (rekursiv) in einfacher Genauigkeit berechnet (siehe snTask) und ausgegeben
+    print(f"In doppelter Genauigkeit: {sn.doublePrecision(res1Rec)}")  # SN1 (rekursiv) in doppelter Genauigkeit berechnet (siehe snTask) und ausgegeben
+    """
