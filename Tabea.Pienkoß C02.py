210c0346e7267f96b53ceff27a78dfc624e57fa1
Tajao02 <tajao.web@gmx.de>
2023-10-25 17:46:09 +0200

    Keine Zeit mehr nur was getestet für teilaufgabe 3

--- Tabea.Pienkoß C02.py
+++ Tabea.Pienkoß C02.py
@@ -1,6 +1,7 @@
 #Aufgabe C02
 #teilaufgabe 2
-import math
+import numpy as np
+import matplotlib as plt
 def y_n (x, n): #Funktion y_n definieren
     return (x**(2*n))/(7+x**2) #unsere Funktion einsetzen
 
@@ -23,3 +24,25 @@ n = 5   # Anzahl der Subintervalle (rekursive Tiefe)
 
 result = recursive_trapezoidal_integration(lambda x: y_n(x, 2), a, b, n)  # Hier wird n = 2 verwendet
 print(f"Das Integral von y_n(x) von {a} bis {b} für n = 2 ist etwa {result}")
+
+#C02 Teilaufgabe 3
+def y_n(x, n):
+    return (x**(2*n)) / (7 + x**2)
+
+# Erzeugung von x-Werten von 0 bis 1
+x = np.linspace(0, 1, 100)
+
+# Erzeugung von n-Werten
+n_values = [1, 2, 3, 4, 5]
+
+# Erzeugung einer Grafik für jede n-Wert
+for n in n_values:
+    y = y_n(x, n)
+    plt.plot(x, y, label=f'n={n}')
+
+plt.xlabel('x')
+plt.ylabel('y_n(x)')
+plt.title('y_n(x) für verschiedene n')
+plt.legend()
+plt.grid(True)
+plt.show()
